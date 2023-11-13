f7c6677d1696f23c561ca620cab5f7711a6784fb
Silvia Cotroneo <silvia.cotroneo@aalto.fi>
2023-11-13 09:30:51 +0200

    Added centering of signal and changed local files path

--- mne/preprocessing/tests/test_oma.py
+++ mne/preprocessing/tests/test_oma.py
@@ -1,9 +1,19 @@
+#from mne.preprocessing import fix_grad_artifact
+import sys
+import os 
+
+sys.path.append("/home/sfc/coding/mne-python/mne/")
+#import oma
+print(sys.path)
+import mne.io
+
+
 from mne.preprocessing import fix_grad_artifact
-import mne
 
-data_path = "/home/sfc/Documents/Aalto/GRA/lukeminen0058.vhdr"
+data_path = "/home/sfc/coding/data/lukeminen0058.vhdr"
 
-raw = mne.io.read_raw_brainvision(data_path,
+# Should be high passed / centered
+# raw = mne.io.read_raw_brainvision(data_path,
               eog=['Eog1', 'Eog2','Eog3', 'Ekg'], preload = True)
 #raw.plot()
-fix_grad_artifact(raw, 1,  1)
+# fix_grad_artifact(raw, 1,  1)
